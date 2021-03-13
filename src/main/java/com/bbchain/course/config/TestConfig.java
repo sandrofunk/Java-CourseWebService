eadbdbedbb7748872f0fe5db7b9ea5740b0626da
sandrofunk <sagfunk@gmail.com>
2021-03-13 14:07:09 -0300

    Payment, one-to-one association

--- src/main/java/com/bbchain/course/config/TestConfig.java
+++ src/main/java/com/bbchain/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.bbchain.course.entities.Category;
 import com.bbchain.course.entities.Order;
 import com.bbchain.course.entities.OrderItem;
+import com.bbchain.course.entities.Payment;
 import com.bbchain.course.entities.Product;
 import com.bbchain.course.entities.User;
 import com.bbchain.course.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		OrderRepository.save(o1);
 	}
 
 }
