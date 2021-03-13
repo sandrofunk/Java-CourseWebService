b5d814d36560ce2d8aac1887a68d626177d9d233
sandrofunk <sagfunk@gmail.com>
2021-03-12 22:16:25 -0300

    Product

---
+++ src/main/java/com/bbchain/course/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.bbchain.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bbchain.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	
+
+}
