34c91e42b762b9ee9be4d50cc4456be36d694752
sandrofunk <sagfunk@gmail.com>
2021-03-13 13:25:41 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/bbchain/course/entities/Product.java
+++ src/main/java/com/bbchain/course/entities/Product.java
@@ -11,12 +11,14 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -30,6 +32,9 @@ public class Product implements Serializable {
 	@ManyToMany
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 	}
@@ -87,6 +92,15 @@ public class Product implements Serializable {
 		return categories;
 	}
 
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -111,5 +125,4 @@ public class Product implements Serializable {
 			return false;
 		return true;
 	}
-
 }
